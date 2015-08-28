643d8611673e88f45f1fcfd2f7ea2ef047bcfeaf
Ash Skutches <ashskutches@gmail.com>
2015-08-28 11:18:32 -0700

    Test edge case

--- practice.js
+++ practice.js
@@ -13,6 +13,7 @@ test('Reversing strings', function() {
     ok(reverse('Jim Bob') != 'Jim Bob', 'does not return original value');
     ok(reverse('abc') == 'cba', 'reverses plain string');
     ok(reverse('abc def') == 'fed cba', 'handles spaces');
+    ok(reverse('1234567891011121314151617181920212223242526272829303132333435363738394041424344454647484950') == '0594847464544434241404938373635343332313039282726252423222120291817161514131211101987654321', 'handles big string');
 });
 
 
