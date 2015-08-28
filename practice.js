19a19fa4ff4dd7c4443a6a38f99c93cb17c4cff0
Ash Skutches <ashskutches@gmail.com>
2015-08-28 12:29:50 -0700

    Add more rebust test

--- practice.js
+++ practice.js
@@ -10,5 +10,6 @@ test('Reversing sentences', function() {
     ok(reverseSentence('funny') == 'funny', 'Maintains words');
     ok(reverseSentence('Funny') == 'Funny', 'Maintains capitals');
     ok(reverseSentence('Funny Stuff') == 'Stuff Funny', 'Reverses sentences');
+    ok(reverseSentence('Funny Stuff 2113 For My Friends') == 'Friends My For 2113 Stuff Funny', 'Reverses more complex sentences');
 });
 
